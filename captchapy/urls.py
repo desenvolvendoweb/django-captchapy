1e0b57d172d78f4cd3053facefc08de91631e7d8
desenvolvendoweb <rafael.exe@hotmail.com>
2014-11-20 10:26:13 -0200

    Update urls.py
    Adicionado urls para django 1.6.2

--- captchapy/urls.py
+++ captchapy/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import include, patterns, url
+try:
+    from django.conf.urls.defaults import include, patterns, url
+except:
+    # Django 1.6.2
+    from django.conf.urls import include, patterns, url
 from django.conf import settings
 
 import os
